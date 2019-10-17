bd7d07dd9ad2cd7c5fece1b3bcdd4e1cca78fcbf
yunushalit <kashapov@yandexlyceum.ru>
2019-10-17 20:48:21 +0400

    флэшток

--- rusflag.py
+++ rusflag.py
@@ -6,6 +6,8 @@ def run_game():
     sc = pygame.display.set_mode((400, 450))
     bg_color = (220, 220, 255)
     sc.fill(bg_color)
+    # флэшток фигуры
+    pygame.draw.rect(sc, (255, 155, 0), (52, 18, 10, 420))
     pygame.display.flip()
     while True:
         for event in pygame.event.get():
